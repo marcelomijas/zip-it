7a3485c735e92fd8baaa44e14d44617b9a1a860c
Marcelo Moreno <62946623+marcelomijas@users.noreply.github.com>
2024-05-16 18:40:55 +0200

    Add GNU/Linux support

--- zip-it.py
+++ zip-it.py
@@ -1,38 +1,53 @@
-from os import listdir, path
+from os import listdir, path, system, name
 from shutil import make_archive
 
 
 def zipit(wd: str, ze: tuple):
+    """This function compresses all folders found in 'wd' except the ones in 'ze'."""
     for i in listdir(wd):
-        if not path.isfile(wd + "\\" + i) and i not in ze:
-            print(f"Compressing '{i}' to '{i}.zip'")
+        if not path.isfile(wd + "/" + i) and i not in ze:
+            print(f"-> {i}.zip")
             try:
                 make_archive(i, "zip", i)
-                print(" Complete!")
+                print("\tComplete!")
             except:
-                print(" Error!")
+                print("\tError!")
+
+
+def clear():
+    # Windows
+    if name == "nt":
+        system("cls")
+    # GNU/Linux
+    else:
+        system("clear")
 
 
 def main():
-    # get executable working directory
+    # get file working directory
     fp = path.abspath(__file__)
     wd = path.dirname(fp)
-    # proceed check
-    print("This is the current working directory: ")
-    print(wd)
-    proceed = input("Do you want to proceed? [Y/n] ")
+    print("Current working directory:", wd)
+    # read exceptions file
+    try:
+        fe = open(wd + "/zip-itExceptions.txt", "r", encoding="utf-8")
+        ze = fe.read().splitlines()
+        fe.close()
+        print("zip-itExceptions.txt file found.")
+    except:
+        ze = ()
+    print("\nThe following folders will be compressed:")
+    # list folders to compress
+    for i in listdir(wd):
+        if not path.isfile(wd + "/" + i) and i not in ze:
+            print(f"-> {i}")
+    proceed = input("\nDo you want to proceed? [Y/n] ")
     if proceed not in ("n", "N", "No", "NO"):
-        # read exceptions file
-        try:
-            fe = open(wd + "\\zip-itExceptions.txt", "r", encoding="utf-8")
-            ze = fe.read().splitlines()
-            fe.close()
-        except:
-            ze = ()
-        # zip-it!
+        clear()
+        print("Compressing:")
         zipit(wd, ze)
-        # finish
-        input("Press Enter to finish the program or close this window ")
+    # finish
+    input("\nPress Enter to finish the program or close this window ")
 
 
 main()
